fc23a296d7bc318a3dd391b3666e87985463fe56
markmead <itsmarkmead@gmail.com>
2020-12-07 21:10:20 +0000

    rubocop cleanup

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,7 +2,7 @@ class RegistrationsController < Devise::RegistrationsController
   before_action :configure_account_update_params, only: :update
   before_action :configure_sign_up_params, only: :create
 
-  protected
+protected
 
   def configure_account_update_params
     devise_parameter_sanitizer.permit(:account_update, keys: %w[email current_password password password_confirmation username])
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,8 +4,8 @@ ActsAsTaggableOn::Tag.destroy_all
 ActiveRecord::Base.connection.reset_pk_sequence!("posts")
 ActiveRecord::Base.connection.reset_pk_sequence!("user")
 
-first_user = User.create!(email: "first_user@test.com", username: 'First User', password: "test123", password_confirmation: "test123")
-second_user = User.create!(email: "second_user@test.com", username: 'Second User', password: "test123", password_confirmation: "test123")
+first_user = User.create!(email: "first_user@test.com", username: "First User", password: "test123", password_confirmation: "test123")
+second_user = User.create!(email: "second_user@test.com", username: "Second User", password: "test123", password_confirmation: "test123")
 
 Post.create!(title: "Aggressive Formatter", user_id: first_user.id, live: true, tag_list: %w[formatter aggressive])
 Post.create!(title: "Careful", user_id: second_user.id, live: true, tag_list: %w[careful])
